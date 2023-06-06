48ac78329acfa7e317b1bfc8df691e0a411e1221
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-06 16:16:48 +0500

    Adding setup files

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,7 @@
+class GreetingsController < ApplicationController
+    def random_greeting
+      random_message = Message.order("RANDOM()").first
+      render json: { greeting: random_message&.content }
+    end
+  end
+  
