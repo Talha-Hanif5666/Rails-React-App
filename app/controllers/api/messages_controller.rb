492f0312e82c3eb2fe9343ebdb2bd6c0192dd803
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-09 16:34:14 +0500

    Remove linter errors

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,6 @@
 class Api::MessagesController < ApplicationController
-    def index
-      @message = Message.order("RANDOM()").first
-      @greeting = @message.content
-    end
+  def index
+    @message = Message.order('RANDOM()').first
+    @greeting = @message.content
   end
-  
+end
